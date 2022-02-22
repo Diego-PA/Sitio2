e06ada4046df0afe174ae4aad7754d4e5cc97c5c
diego <diego.loge@hotmail.com>
2022-02-22 07:11:17 -0600

    commit inicial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <title>Diego Puebla Aldama</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1">Diego Puebla Aldama</span>
+    </nav>
+    <br>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Información personal</h5>
+                <p class="card-text">
+                    Hola, soy Diego Puebla Aldama, estudio informática y tengo 22 años, vivo en la CDMX y me gusta escuchar música, jugar videojuegos y cocinar. 
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
